7dff6c4f102d6378b15d33f9e8f66fbe7055be2c
rohitharavinder <rohitha0112@gmail.com>
2023-01-13 18:54:53 +0100

    Added code to extract uniref cluster ids using API

--- code/uniref_api.py
+++ code/uniref_api.py
@@ -44,7 +44,7 @@ def merge_outputs(first_file: str, second_file: str, output_file: str) -> None:
 
 if __name__=="__main__":
     url1 = "https://rest.uniprot.org/uniref/stream?compressed=true&fields=id%2Cname%2Ctypes%2Ccount&format=tsv&query=%28%28taxonomy_id%3A2759%29%20AND%20%28count%3A%5B2%20TO%202%5D%29%29%20AND%20%28identity%3A0.9%29"
-    url2 = "https://rest.uniprot.org/uniref/stream?compressed=true&fields=id%2Cname%2Ctypes%2Ccount&format=tsv&query=%28%28taxonomy_id%3A2759%29%20AND%20%28identity%3A0.9%29%20AND%20%28count%3A%5B3%20TO%20%2A%5D%29%29""
+    url2 = "https://rest.uniprot.org/uniref/stream?compressed=true&fields=id%2Cname%2Ctypes%2Ccount&format=tsv&query=%28%28taxonomy_id%3A2759%29%20AND%20%28identity%3A0.9%29%20AND%20%28count%3A%5B3%20TO%20%2A%5D%29%29"
     request_api(url1, "./data/uniref/uniref_ids_1.tsv")
     request_api(url2, "./data/uniref/uniref_ids_2.tsv")
     merge_outputs("./data/uniref/uniref_ids_1.tsv", "./data/uniref/uniref_ids_2.tsv", "./data/uniref/uniref_ids.tsv")
