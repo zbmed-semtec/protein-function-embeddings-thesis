857635ddb1756e4426808630b25ab40631936c20
rohitharavinder <rohitha0112@gmail.com>
2023-01-13 17:57:06 +0100

    Modified code for formatting embeddings

--- code/format_embeddings.py
+++ code/format_embeddings.py
@@ -24,7 +24,7 @@ def load_embeddings(embeddings_dir: str) -> Tuple[list, list]:
         accession = embedding_file.split(".npy")[0]
         embedding = np.load(f'{embeddings_dir}/{embedding_file}', allow_pickle=True)
         accessions.append(accession)
-        embeddings.append(np.array(embedding, dtype=np.float16))
+        embeddings.append(np.array(embedding, dtype=np.float32))
     return accessions, embeddings
 
 
@@ -43,7 +43,10 @@ def save_embeddings(accessions: list, embeddings: list, output_pickle_path: str)
     """
     embeddings_df = pd.DataFrame({"accessions": accessions, "embeddings": embeddings})
     embeddings_df.sort_values(by="accessions", ignore_index=True, inplace=True)
-    embeddings_df.to_pickle(output_pickle_path)
+    embeddings_df.drop(embeddings_df[embeddings_df['accessions'] == 'P19664'].index, inplace=True)
+    embeddings_df.drop(embeddings_df[embeddings_df['accessions'] == 'P22972'].index, inplace=True)
+    embeddings_df.to_pickle(output_pickle_path, protocol=4)
+    print("Saved embeddings")
 
 
 def load_embeddings_pickle(input_pickle_path: str) -> pd.DataFrame:
@@ -63,6 +66,83 @@ def load_embeddings_pickle(input_pickle_path: str) -> pd.DataFrame:
 
 
 if __name__ == "__main__":
-    all_accessions, all_embeddings = load_embeddings("./data/output/embeddings/word2doc2vec/cbow/min_count_2")
-    save_embeddings(all_accessions, all_embeddings, "./data/output/embeddings_pickle/word2doc2vec_embs_1.pkl")
-    load_embeddings_pickle("./data/output/embeddings_pickle/word2doc2vec_embs_1.pkl")
+
+    # WORD2DOC2VEC 200
+    all_accessions, all_embeddings = load_embeddings("./data/embeddings/word2doc2vec/200/cbow/min_count_2/")
+    save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/word2doc2vec_embs_euk_1.pkl")
+    print("Embeddings formatted 1")
+    all_accessions, all_embeddings = load_embeddings("./data/embeddings/word2doc2vec/200/cbow/min_count_3/")
+    save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/word2doc2vec_embs_euk_2.pkl")
+    print("Embeddings formatted 2")
+    all_accessions, all_embeddings = load_embeddings("./data/embeddings/word2doc2vec/200/cbow/min_count_4/")
+    save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/word2doc2vec_embs_euk_3.pkl")
+    print("Embeddings formatted 3")
+    all_accessions, all_embeddings = load_embeddings("./data/embeddings/word2doc2vec/200/sg/min_count_2/")
+    save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/word2doc2vec_embs_euk_4.pkl")
+    print("Embeddings formatted 4")
+    all_accessions, all_embeddings = load_embeddings("./data/embeddings/word2doc2vec/200/sg/min_count_3/")
+    save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/word2doc2vec_embs_euk_5.pkl")
+    print("Embeddings formatted 5")
+    all_accessions, all_embeddings = load_embeddings("./data/embeddings/word2doc2vec/200/sg/min_count_4/")
+    save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/word2doc2vec_embs_euk_6.pkl")
+    print("Embeddings formatted 6")
+
+    # WORD2DOC2VEC 400
+    all_accessions, all_embeddings = load_embeddings("./data/embeddings/word2doc2vec/400/cbow/min_count_2/")
+    save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/word2doc2vec_embs_euk_7.pkl")
+    print("Embeddings formatted 7")
+    all_accessions, all_embeddings = load_embeddings("./data/embeddings/word2doc2vec/400/cbow/min_count_3/")
+    save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/word2doc2vec_embs_euk_8.pkl")
+    print("Embeddings formatted 8")
+    all_accessions, all_embeddings = load_embeddings("./data/embeddings/word2doc2vec/400/cbow/min_count_4/")
+    save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/word2doc2vec_embs_euk_9.pkl")
+    print("Embeddings formatted 9")
+    all_accessions, all_embeddings = load_embeddings("./data/embeddings/word2doc2vec/400/sg/min_count_2/")
+    save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/word2doc2vec_embs_euk_10.pkl")
+    print("Embeddings formatted 10")
+    all_accessions, all_embeddings = load_embeddings("./data/embeddings/word2doc2vec/400/sg/min_count_3/")
+    save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/word2doc2vec_embs_euk_11.pkl")
+    print("Embeddings formatted 11")
+    all_accessions, all_embeddings = load_embeddings("./data/embeddings/word2doc2vec/400/sg/min_count_4/")
+    save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/word2doc2vec_embs_euk_12.pkl")
+    print("Embeddings formatted 12")
+
+    # HYBRID 200
+    # all_accessions, all_embeddings = load_embeddings("./data/embeddings/hybrid/200/cbow/min_count_2/")
+    # save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/hybrid_embs_euk_1.pkl")
+    # print("Embeddings formatted 1")
+    # all_accessions, all_embeddings = load_embeddings("./data/embeddings/hybrid/200/cbow/min_count_3/")
+    # save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/hybrid_embs_euk_2.pkl")
+    # print("Embeddings formatted 2")
+    # all_accessions, all_embeddings = load_embeddings("./data/embeddings/hybrid/200/cbow/min_count_4/")
+    # save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/hybrid_embs_euk_3.pkl")
+    # print("Embeddings formatted 3")
+    # all_accessions, all_embeddings = load_embeddings("./data/embeddings/hybrid/200/sg/min_count_2/")
+    # save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/hybrid_embs_euk_4.pkl")
+    # print("Embeddings formatted 4")
+    # all_accessions, all_embeddings = load_embeddings("./data/embeddings/hybrid/200/sg/min_count_3/")
+    # save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/hybrid_embs_euk_5.pkl")
+    # print("Embeddings formatted 5")
+    # all_accessions, all_embeddings = load_embeddings("./data/embeddings/hybrid/200/sg/min_count_4/")
+    # save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/hybrid_embs_euk_6.pkl")
+    # print("Embeddings formatted 6")
+
+    # HYBRID 400
+    # all_accessions, all_embeddings = load_embeddings("./data/embeddings/hybrid/400/cbow/min_count_2/")
+    # save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/hybrid_embs_euk_7.pkl")
+    # print("Embeddings formatted 7")
+    # all_accessions, all_embeddings = load_embeddings("./data/embeddings/hybrid/400/cbow/min_count_3/")
+    # save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/hybrid_embs_euk_8.pkl")
+    # print("Embeddings formatted 8")
+    # all_accessions, all_embeddings = load_embeddings("./data/embeddings/hybrid/400/cbow/min_count_4/")
+    # save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/hybrid_embs_euk_9.pkl")
+    # print("Embeddings formatted 9")
+    # all_accessions, all_embeddings = load_embeddings("./data/embeddings/hybrid/400/sg/min_count_2/")
+    # save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/hybrid_embs_euk_10.pkl")
+    # print("Embeddings formatted 10")
+    # all_accessions, all_embeddings = load_embeddings("./data/embeddings/hybrid/400/sg/min_count_3/")
+    # save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/hybrid_embs_euk_11.pkl")
+    # print("Embeddings formatted 11")
+    # all_accessions, all_embeddings = load_embeddings("./data/embeddings/hybrid/400/sg/min_count_4/")
+    # save_embeddings(all_accessions, all_embeddings, "./data/emb_pickle/hybrid_embs_euk_12.pkl")
+    # print("Embeddings formatted 12")
